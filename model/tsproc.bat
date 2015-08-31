f39b1e6681a23be90e66e175e40ed27cc866a0a3
kbrannan <brannan.kevin@deq.state.or.us>
2015-08-31 16:10:58 -0700

    I got tsproc.bat to work from the shell command of R

---
+++ model/tsproc.bat
@@ -0,0 +1,2 @@
+cd M:\Models\Bacteria\HSPF\HydroCal201506\R_projs\Check_upd_wdm_hyd\model
+type tsproc.in | tsproc.exe
--- run_tsproc.R
+++ run_tsproc.R
@@ -1,24 +1,9 @@
-
-shell.exec(cat("tsproc ",getwd(),"/model/trproc.dat", "\n n \n n"))
-Sys.getenv("PATH")
-shell("tsproc")
-shell("pest")
-
-shell(cmd=paste0(getwd(),"/model/model.bat"), shell="cmd", translate=TRUE)
-
-shell.exec(paste0(getwd(),"/model/model.bat"))
-
-shell(cmd="model.bat",shell="cmd")
-
-shell(cmd=paste0(getwd(),"/model/tsproc.bat > NULL"))
-
-getwd()
-
-shell(cmd="cmd /c M:\Models\Bacteria\HSPF\HydroCal201506\R_projs\Check_upd_wdm_hyd\model\\\model.bat")
-
-shell.exec("tsproc.exe")
-
-shell(cmd=paste0("tsproc.exe ", paste0(getwd(),"/model/tsproc.dat")," n n"))
-
+# had to modify tsproc.bat from how Cadmus had it set up. 
+# this doest not work from R: tsproc.exe < tsproc.in > null
+# for some reason the redirect for input "<" would not work
+# I used the commands:
+# cd M:\Models\Bacteria\HSPF\HydroCal201506\R_projs ...
+#       \Check_upd_wdm_hyd\model
+# type tsproc.in | tsproc.exe
+# the pipe "|" of the output from "type" to tsproc.exe works
 shell(cmd="tsproc.bat")
-shell(cmd="path")
